3c74e7810ab934bb28f06461c83180924b0241dd
Mr Jahnke <jahnkej@ask.edu.kw>
2017-03-15 12:05:57 +0300

    Mar15

--- Elevens/Shuffler.java
+++ Elevens/Shuffler.java
@@ -42,7 +42,7 @@ public class Shuffler {
         System.out.println();
         
                 System.out.println("Results of " + SHUFFLE_COUNT +
-                                 " consecutive efficient selection shuffles:");
+                                 " consecutive LESS efficient selection shuffles:");
         int[] values3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int j = 1; j <= SHUFFLE_COUNT; j++) {
             selectionShuffle3(values3);
@@ -53,6 +53,25 @@ public class Shuffler {
             System.out.println();
         }
         System.out.println();
+
+        System.out.println("Results of " + SHUFFLE_COUNT +
+                                 " flips:");
+        
+        for (int j = 1; j <= SHUFFLE_COUNT; j++) {
+            System.out.println(flip());
+        }
+        System.out.println();
+        
+        int[] numbers1 = {0,1,2,3,4};
+        int[] numbers2 = {4,2,3,1,0};
+        int[] numbers3 = {1,2,3,4,5};
+        
+        System.out.println("Numbers1 and Numbers2 are permutations2: " + arePermutations2(numbers1, numbers2));
+        System.out.println("Numbers1 and Numbers3 are permutations2: " + arePermutations2(numbers1, numbers3));
+        System.out.println();
+        System.out.println("Numbers1 and Numbers2 are permutations: " + arePermutations(numbers1, numbers2));
+        System.out.println("Numbers1 and Numbers3 are permutations: " + arePermutations(numbers1, numbers3));
+
     }
 
 
@@ -146,6 +165,63 @@ public class Shuffler {
             values[j] = shuffled[j];
         }
         System.out.println("Loops: "+count);
+    }
+    
+    public static String flip()
+    {
+        int ran = (int)(Math.random()*3);
+        String flip = "heads";
+        if (ran == 2)
+            flip = "tails";
+        return flip;
+    }
+    
+    public static boolean arePermutations2 (int[] arr1, int[] arr2)
+    {
+        if (arr1.length != arr2.length)
+            return false;
+        boolean found = false;
+        for (int j=0; j<arr2.length; j++)
+        { 
+                if (!contains(arr1, arr2[j]))
+                    return false;
+        }       
+        return true;
+    }
+    
+    
+    private static boolean contains(int[] arr, int n)
+    {
+        for (int j=0; j<arr.length; j++)
+        {
+            if (arr[j]==n)
+                return true;
+        }
+        return false;
+    }
+    
+    public static boolean arePermutations(int[] arr1, int[] arr2)
+    {
+        if (arr1.length != arr2.length)
+            return false;
+        boolean found = false;
+        for (int j=0; j<arr1.length; j++)
+        { 
+                for (int k=0; k<arr2.length; k++)
+                {
+                    if (arr1[j]==arr2[k])
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    found = false;  // or return false;
+                    break;
+                }
+        }       
+        return found;  // or return true;
     }
 }
  
